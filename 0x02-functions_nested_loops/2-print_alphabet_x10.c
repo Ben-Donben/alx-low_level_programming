17711215637a88bbb094d4bd0bd30d6148ad86c4
Ben-Donben <agbakabenedictdonben@yahoo.com>
2023-02-22 13:53:15 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,19 +6,18 @@
  */
 
 void print_alphabet_x10(void)
-        
+
 {
-
 	char c;
-        int i = 0;
+	int i = 0;
 
-        while (i <= 9)
-        {
-        for (c = 'a'; c <= 'z'; c++)
-        {
-        _putchar(c);
-        }
-        _putchar('\n');
-        i++;
-        }
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
 }
