fe92d4ee2da1a1594b8e76fc4abb7285876bf2a7
Ben-Donben <agbakabenedictdonben@yahoo.com>
2023-02-27 23:22:21 -0800

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Tasks a pointer to an int and updates
+ * the value it points to to 98
+ *
+ * @n: An integer input pointer
+ *
+ * Return : noting
+ */
+void reset_to_98(int *n)
+{
+
+		*n = 98;
+
+}
