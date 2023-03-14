efb67bd83a70ffd06f4cffbd19164fb921ab64d0
Ben-Donben <agbakabenedictdonben@yahoo.com>
2023-03-14 06:30:46 -0700

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,7 +7,7 @@ char **strtow(char *str);
 
 /**
  * word_len - Locates the index marking the end of the
- * 		first word contained within a string.
+ * first word contained within a string.
  * @str: The string to be searched.
  *
  * Return: The index marketing the end of the initial word pointed to by str.
@@ -55,7 +55,7 @@ int count_words(char *str)
  * @str: The string to be split.
  *
  * Return: If str = NULL, str = "", or the function fails - NULL.
- * 	Otherwise - a pointer to an array of strings (words).
+ * Otherwise - a pointer to an array of strings (words).
  */
 char **strtow(char *str)
 {
