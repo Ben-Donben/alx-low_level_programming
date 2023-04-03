00a1090900613a373825e2a92180468f19962778
Ben-Donben <agbakabenedictdonben@yahoo.com>
2023-04-03 14:32:42 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,7 +11,7 @@ size_t print_listint(const listint_t *h)
 	size_t num = 0;
 
 	while (h)
-	{
+ 	{
 		printf("%d\n", h->n);
 		num++;
 		h = h->next;
@@ -19,4 +19,3 @@ size_t print_listint(const listint_t *h)
 
 	return (num);
 }
-
